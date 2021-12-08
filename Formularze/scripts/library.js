4ee08a7d45e8dd3e3ed10a879e54dea5b76c8111
PATRYK KAMUSINSKI <theneonxd@gmail.com>
2021-12-08 03:01:00 +0100

    task 2 styling

--- Formularze/scripts/library.js
+++ Formularze/scripts/library.js
@@ -1,22 +1,16 @@
 export class Library {
     addBook(book) {
         const htmlBook = this.createBook(book);
-        const container = document.getElementById('library');
-        container.appendChild(htmlBook);
+        const table = document.getElementById('libraryTableBody');
+        table.appendChild(htmlBook);
     }
     createBook(book) {
-        const htmlBook = document.createElement('div');
-        const htmlTitle = document.createElement('span');
-        const htmlAuthor = document.createElement('span');
-        const htmlPriority = document.createElement('span');
-        const htmlCategory = document.createElement('span');
-
-        htmlBook.classList.add('book');
-        htmlTitle.classList.add('longFields');
-        htmlAuthor.classList.add('longFields');
-        htmlPriority.classList.add('shortFields');
-        htmlCategory.classList.add('longFields');
-
+        const htmlBook = document.createElement('tr');
+        const htmlTitle = document.createElement('td');
+        const htmlAuthor = document.createElement('td');
+        const htmlPriority = document.createElement('td');
+        const htmlCategory = document.createElement('td');
+        
         htmlTitle.innerHTML = book.title;
         htmlAuthor.innerHTML = book.author;
         htmlPriority.innerHTML = book.priority;
